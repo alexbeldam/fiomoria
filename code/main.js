e4009196e044e34f7a3cd9485281feae37361119
Alex <106851123+lexhtml@users.noreply.github.com>
2022-11-01 19:37:40 -0300

    Update main.js

--- code/main.js
+++ code/main.js
@@ -3,8 +3,11 @@ const buttons = document.querySelectorAll('h2');
 
 firstButton.addEventListener('click', () => {
     firstButton.classList.add('kd');
-    firstButton.style.display = 'none'
+    firstButton.style.display = 'none';
 
     for (let button of buttons)
+    {
+        button.style.display = 'block';
         button.classList.remove('kd');
+    }
 });
