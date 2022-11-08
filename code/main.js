d5ef45131d84e872fd6b7725924176e527d45ddc
Alex <106851123+lexhtml@users.noreply.github.com>
2022-11-08 17:07:51 -0300

    Update main.js

--- code/main.js
+++ code/main.js
@@ -1,13 +1,20 @@
 const firstButton = document.querySelector('h1');
 const buttons = document.querySelectorAll('h2');
+const links = ['fionencia/', 'shrek-wiki/', 'credits/']; 
 
-firstButton.addEventListener('click', () => {
+function buttonChange() {
     firstButton.classList.add('kd');
     firstButton.style.display = 'none';
-
+    
     for (let button of buttons)
     {
         button.style.display = 'block';
         button.classList.remove('kd');
     }
-});
+}
+
+firstButton.addEventListener('click', buttonChange);
+setTimeout(buttonChange, 10000);
+
+for (let i = 0; i < 3; i++)
+    buttons[i].addEventListener('click', () => location.href = links[i]);
