7d8440fb716f209857d01e9c77b313851d71ad3b
Alex <alexthebelle@yahoo.com>
2022-11-28 11:24:36 -0300

    a header mais difícil do mundo

--- fiomoria/index.php
+++ fiomoria/index.php
@@ -11,12 +11,37 @@ include('code/protect.php');
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>fiomória</title>
+    <title>fiomória | olá, <?php echo $_SESSION['user'] ?>!</title>
     <link rel="icon" href="img/icon.png">
     <link rel="stylesheet" href="style/fiomoria.css">
+    <link rel="stylesheet" href="style/balao.css">
 </head>
 
 <body>
+    <header>
+        <ul>
+            <li>
+                <select name="modo de jogo">
+                    <option value="" disabled selected>Selecione o modo de jogo</option>
+                    <option value="0">Facílimo</option>
+                    <option value="1">Fácil</option>
+                    <option value="2">Médio</option>
+                    <option value="3">Difícil</option>
+                    <option value="4">Se coloca no lugar dela</option>
+                </select>
+            </li>
+            <li>
+                <button id="joga">Jogar</button>
+            </li>
+            <li>
+                <a href="../">Início</a>
+            </li>
+            <li>
+                <a href="code/logout">Deslogar</a>
+            </li>
+        </ul>
+    </header>
+
     <section class="fiomoria-game">
         <div class="fiomoria-carta" data-framework="fionaHumana">
             <img class="frente" src="img/fionaHumana.jpg" alt="Fiona Humana">
