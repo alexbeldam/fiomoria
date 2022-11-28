8ea96006a9324f7942cde478e5bc8492d5c09a76
Alex Belle <alexthebelle@yahoo.com>
2022-11-28 17:48:05 -0300

    novo código fonte fiomoria

--- fiomoria/code/fiomoria.js
+++ fiomoria/code/fiomoria.js
@@ -1,67 +1,123 @@
-let cartas = document.querySelectorAll('.fiomoria-carta');
-let temCartaVirada = false;
-let bloqueio = false;
-let primeiraCarta, segundaCarta;
+const select = document.querySelector('select[name="game-mode"');
+const play = document.querySelector('#joga');
+const board = document.querySelector('.fiomoria-game');
+const characters = [
+    'burro',
+    'burroAlazao',
+    'dragao',
+    'encantado',
+    'fada',
+    'farquaad',
+    'fiona',
+    'fionaHumana',
+    'gato',
+    'rumple',
+    'shrek',
+    'shrekHumano'
+];
+let first = '';
+let second = '';
 
-function viraCarta() 
-{
-    if(bloqueio)
-        return;
+function novoElemento (tag, classe) {
+    const el = document.createElement(tag);
+    el.className = classe;
 
-    if(this === primeiraCarta)
-        return;
-
-    this.classList.add('flip');
-
-    if(!temCartaVirada) 
-    {
-       temCartaVirada = true;
-       primeiraCarta = this;
-       return;
-    }
-
-    segundaCarta = this;
-    bloqueio = true;
-
-    checar();
+    return el;
 }
 
-function checar() 
-{
-    let par =  primeiraCarta.dataset.framework === segundaCarta.dataset.framework;
-    par ? desvira() : volta();
+function criaCarta(character) {
+    const carta = novoElemento('figure', 'fiomoria-carta');
+    const frente = novoElemento('figure', 'face frente');
+    const verso = novoElemento('figure', 'face verso');
+
+    frente.style.backgroundImage = `url("img/${character}.jpg")`;
+
+    carta.appendChild(frente);
+    carta.appendChild(verso);
+    carta.setAttribute('data-character', character)
+
+    board.appendChild(carta);
+
+    return carta
 }
 
-function desvira()
-{
-    primeiraCarta.removeEventListener('click', viraCarta());
-    segundaCarta.removeEventListener('click', viraCarta());
-    reseta();
+function close() {
+    first.removeEventListener('click', e => flip(e));
+    second.removeEventListener('click', e => flip(e));
+    const firstFrente = first.firstChild;
+    const secondFrente = second.firstChild;
+
+    firstFrente.classList.add('disabled');
+    secondFrente.classList.add('disabled');
+
+    first = '';
+    second = '';
 }
 
-function volta()
-{
-    bloqueio = true;
-
+function unflip() {
     setTimeout(() => {
-        primeiraCarta.classList.remove('flip');
-        segundaCarta.classList.remove('flip');
-        reseta();
-    }, 1000);
+        first.classList.remove('flip');
+        second.classList.remove('flip');
+
+        first = '';
+        second = '';
+    }, 1000)
 }
 
-function reseta() 
-{
-    [temCartaVirada, bloqueio] = [false, false];
-    [primeiraCarta, segundaCarta] = [null, null];
+function checar() {
+    par = first.getAttribute('data-character') === second.getAttribute('data-character');
+
+    par ? close() : unflip();
 }
 
-(function embaralhar()
-{
+function flip(evt) {
+    const target = evt.target;
+    const carta = target.parentNode;
+
+    if (carta.className.includes('flip'))
+        return;
+    if (first === '') {
+        first = carta;
+
+        carta.classList.add('flip');
+    }
+    else if (second === '') {
+        second = carta;
+        carta.classList.add('flip');
+
+        checar();
+    }
+}
+
+function embaralhar() {
+    const cartas = document.querySelectorAll('.fiomoria-carta');
+
     cartas.forEach(carta => {
-        let posicaoAleatoria = Math.floor(Math.random() * 12);
+        let posicaoAleatoria = Math.floor(Math.random() * 24);
         carta.style.order = posicaoAleatoria;
     });
-})();
+}
 
-cartas.forEach(carta => carta.addEventListener('click', viraCarta));
+function load(dificuldade) {
+    const duplicateCharacters = [...characters, ...characters];
+
+    duplicateCharacters.forEach(character => {
+        const carta = criaCarta(character);
+
+        carta.addEventListener('click', flip);
+    })
+
+    embaralhar();
+}
+
+play.addEventListener('click', () => {
+    if (select.value === '') {
+        select.classList.add('tryagain');
+
+        setTimeout(() => select.classList.remove('tryagain'), 1500);
+
+        return;
+    }
+    else 
+        load(select.value);
+});
--- fiomoria/index.php
+++ fiomoria/index.php
@@ -13,15 +13,15 @@ include('code/protect.php');
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>fiomória | olá, <?php echo $_SESSION['user'] ?>!</title>
     <link rel="icon" href="img/icon.png">
-    <link rel="stylesheet" href="style/fiomoria.css">
-    <link rel="stylesheet" href="style/balao.css">
+    <link rel="stylesheet" href="style/page.css">
+    <link rel="stylesheet" href="style/game.css">
 </head>
 
 <body>
     <header>
         <ul>
             <li>
-                <select name="modo de jogo">
+                <select name="game-mode">
                     <option value="" disabled selected>Selecione o modo de jogo</option>
                     <option value="0">Facílimo</option>
                     <option value="1">Fácil</option>
@@ -33,136 +33,12 @@ include('code/protect.php');
             <li>
                 <button id="joga">Jogar</button>
             </li>
-            <li>
-                <a href="../">Início</a>
-            </li>
-            <li>
-                <a href="code/logout">Deslogar</a>
-            </li>
+            <li onclick="location.href = '../'"><p>Início</p></li>
+            <li onclick="location.href = 'code/logout'"><p>Deslogar</p></li>
         </ul>
     </header>
 
-    <section class="fiomoria-game">
-        <div class="fiomoria-carta" data-framework="fionaHumana">
-            <img class="frente" src="img/fionaHumana.jpg" alt="Fiona Humana">
-            <img class="verso" src="img/verso.jpeg" alt="verso da carta">
-        </div>
-
-        <div class="fiomoria-carta" data-framework="fionaHumana">
-            <img class="frente" src="img/fionaHumana.jpg" alt="Fiona Humana">
-            <img class="verso" src="img/verso.jpeg" alt="verso da carta">
-        </div>
-
-        <div class="fiomoria-carta" data-framework="burro">
-            <img class="frente" src="img/burro.jpg" alt="Burro">
-            <img class="verso" src="img/verso.jpeg" alt="verso da carta">
-        </div>
-
-        <div class="fiomoria-carta" data-framework="burro">
-            <img class="frente" src="img/burro.jpg" alt="Burro">
-            <img class="verso" src="img/verso.jpeg" alt="verso da carta">
-        </div>
-
-        <div class="fiomoria-carta" data-framework="burroAlazao">
-            <img class="frente" src="img/burroAlazao.jpg" alt="Burro alazão">
-            <img class="verso" src="img/verso.jpeg" alt="verso da carta">
-        </div>
-
-        <div class="fiomoria-carta" data-framework="burroAlazao">
-            <img class="frente" src="img/burroAlazao.jpg" alt="Burro alazão">
-            <img class="verso" src="img/verso.jpeg" alt="verso da carta">
-        </div>
-
-        <div class="fiomoria-carta" data-framework="fiona">
-            <img class="frente" src="img/fiona.jpg" alt="Fiona">
-            <img class="verso" src="img/verso.jpeg" alt="verso da carta">
-        </div>
-
-        <div class="fiomoria-carta" data-framework="fiona">
-            <img class="frente" src="img/fiona.jpg" alt="Fiona">
-            <img class="verso" src="img/verso.jpeg" alt="verso da carta">
-        </div>
-
-        <div class="fiomoria-carta" data-framework="shrek">
-            <img class="frente" src="img/shrek.jpg" alt="Shrek">
-            <img class="verso" src="img/verso.jpeg" alt="verso da carta">
-        </div>
-
-        <div class="fiomoria-carta" data-framework="shrek">
-            <img class="frente" src="img/shrek.jpg" alt="Shrek">
-            <img class="verso" src="img/verso.jpeg" alt="verso da carta">
-        </div>
-
-        <div class="fiomoria-carta" data-framework="shrekHumano">
-            <img class="frente" src="img/shrekHumano.jpg" alt="Shrek humano">
-            <img class="verso" src="img/verso.jpeg" alt="verso da carta">
-        </div>
-
-        <div class="fiomoria-carta" data-framework="shrekHumano">
-            <img class="frente" src="img/shrekHumano.jpg" alt="Shrek humano">
-            <img class="verso" src="img/verso.jpeg" alt="verso da carta">
-        </div>
-
-        <div class="fiomoria-carta" data-framework="farquaad">
-            <img class="frente" src="img/farquaad.jpg" alt="Farquaad">
-            <img class="verso" src="img/verso.jpeg" alt="verso da carta">
-        </div>
-
-        <div class="fiomoria-carta" data-framework="farquaad">
-            <img class="frente" src="img/farquaad.jpg" alt="Farquaad">
-            <img class="verso" src="img/verso.jpeg" alt="verso da carta">
-        </div>
-
-        <div class="fiomoria-carta" data-framework="fada">
-            <img class="frente" src="img/fada.jpg" alt="Fada madrinha">
-            <img class="verso" src="img/verso.jpeg" alt="verso da carta">
-        </div>
-
-        <div class="fiomoria-carta" data-framework="fada">
-            <img class="frente" src="img/fada.jpg" alt="Fada madrinha">
-            <img class="verso" src="img/verso.jpeg" alt="verso da carta">
-        </div>
-
-        <div class="fiomoria-carta" data-framework="gato">
-            <img class="frente" src="img/gato.jpg" alt="Gato de botas">
-            <img class="verso" src="img/verso.jpeg" alt="verso da carta">
-        </div>
-
-        <div class="fiomoria-carta" data-framework="gato">
-            <img class="frente" src="img/gato.jpg" alt="Gato de botas">
-            <img class="verso" src="img/verso.jpeg" alt="verso da carta">
-        </div>
-
-        <div class="fiomoria-carta" data-framework="rumple">
-            <img class="frente" src="img/rumple.jpg" alt="Rumpel Stiltskin">
-            <img class="verso" src="img/verso.jpeg" alt="verso da carta">
-        </div>
-
-        <div class="fiomoria-carta" data-framework="rumple">
-            <img class="frente" src="img/rumple.jpg" alt="Rumpel Stiltskin">
-            <img class="verso" src="img/verso.jpeg" alt="verso da carta">
-        </div>
-
-        <div class="fiomoria-carta" data-framework="dragao">
-            <img class="frente" src="img/dragao.jpg" alt="Dragão">
-            <img class="verso" src="img/verso.jpeg" alt="verso da carta">
-        </div>
-
-        <div class="fiomoria-carta" data-framework="dragao">
-            <img class="frente" src="img/dragao.jpg" alt="Dragão">
-            <img class="verso" src="img/verso.jpeg" alt="verso da carta">
-        </div>
-
-        <div class="fiomoria-carta" data-framework="encantado">
-            <img class="frente" src="img/encantado.jpg" alt="Encantado">
-            <img class="verso" src="img/verso.jpeg" alt="verso da carta">
-        </div>
-
-        <div class="fiomoria-carta" data-framework="encantado">
-            <img class="frente" src="img/encantado.jpg" alt="Encantado">
-            <img class="verso" src="img/verso.jpeg" alt="verso da carta">
-        </div>
-    </section>
+    <section class="fiomoria-game"></section>
     
     <script src="code/fiomoria.js"></script>
 </body>
